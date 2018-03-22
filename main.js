58791a452d1d7733b23d081593dfbda60c2a0585
Maxime Burlat <maximeburlat@gmail.com>
2018-03-21 22:47:44 -0500

    addition works

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-function Phx(quantity){
-this.value = quantity.match(/1/);
-    console.log(this.value);
+var Phx = {
+    add: function(a,b){
+        if (a.unit === b.unit){
+            let value = a.value + b.value;
+            let unit = a.unit;
+            console.log((value + ' ' + unit));
+            return (Qu(value + ' ' + unit));
+        } else {
+            console.log('unit consistency is wrong');
+            return false;
+        }
+        
+    }
 }
 
-test= new Phx('12.1 mg');
+function Qu(quantity){
+this.value = parseFloat(quantity.match(/\d*\.\d*/)[0]);
+this.unit = quantity.match(/[a-zA-Z\/]+/)[0];
+return {value: this.value, unit: this.unit};
+}
+
+test= new Qu('12.1 mg');
+test2= new Qu('12.1 mg/mL');
+
+console.log(Phx.add(test,test2));
+
